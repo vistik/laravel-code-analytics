87f546953fcccd3c31ef0f307e47a41039ef8f5b
vistik <354169+vistik@users.noreply.github.com>
2026-04-05 22:40:40 +0000

    Fix styling

--- src/FileSignal/FileSignalScoring.php
+++ src/FileSignal/FileSignalScoring.php
@@ -5,9 +5,9 @@ namespace Vistik\LaravelCodeAnalytics\FileSignal;
 interface FileSignalScoring
 {
     /**
-     * @param  array<string, mixed>  $node       The file node (must include 'add', 'del', 'watchLevel')
+     * @param  array<string, mixed>  $node  The file node (must include 'add', 'del', 'watchLevel')
      * @param  list<array<string, mixed>>  $findings  Analysis findings for this file
-     * @param  array<string, mixed>|null  $metrics   PhpMetrics data for this file (cc, mi, ...)
+     * @param  array<string, mixed>|null  $metrics  PhpMetrics data for this file (cc, mi, ...)
      */
     public function calculate(array $node, array $findings, ?array $metrics): int;
 }
